4551ff6e1cec6f73433a6ef68fb72db333730cbe
b <yatsinaba@gmail.com>
2020-03-21 10:55:02 +0200

    вариант реализации hw2 90% точность

--- students/BohdanYatsyna/homework2/task-02-spacy.py
+++ students/BohdanYatsyna/homework2/task-02-spacy.py
@@ -1,23 +1,20 @@
 import spacy
 from spacy import displacy
-import en_core_web_sm
+import en_core_web_lg
 import helpers
 import json
 import os
 from tqdm import tqdm
+import re
 
 class pureHeader:
-    nlp = spacy.load("en_core_web_sm")
+    nlp = spacy.load("en_core_web_md")
 
     def __init__(self, text):
-        #self.nlp = spacy.load("en_core_web_sm")
-        self.modified_text = self.ruleFourLonger(text)
-        self.modified_text = self.ruleCapFirstLast(self.modified_text)
-        self.modified_text = self.ruleForLangParts(self.modified_text)
-        self.modified_text = self.ruleForDash(self.modified_text)
-        self.modified_text = self.ruleForOther(self.modified_text)
+        self.modified_text = self.rules(text)
 
-    def ruleFourLonger(self, text):
+
+    def rules(self, text):
         '''
         Модифікує string таким чином щоб літери слова довжиною 4 були з великої букви.
         :param text: текст
@@ -25,119 +22,93 @@ class pureHeader:
         '''
 
         modified_text = ""
-
-        doc = pureHeader.nlp(text)
-
-        for token in doc:
-            if len(token) > 3:
-                modified_text += token.text.capitalize() + token.whitespace_
-            else:
-                modified_text += token.text + token.whitespace_
-        return modified_text
-
-
-    def ruleCapFirstLast(self, text):
-        '''
-        Модифікує string таким чином щоб літери першого та останньго слова були з великої букви.
-        :param text:
-        :return:
-        '''
-
-        modified_text = ""
         doc = pureHeader.nlp(text)
 
         for i, token in enumerate(doc):
 
-            if i == len(doc) - 1 or i == 0:
-                modified_text += token.text.capitalize() + token.whitespace_
-            else:
-                modified_text += token.text + token.whitespace_
-        return modified_text
-
-
-    def ruleForLangParts(self, text):
-        '''
-        З великої літери потрібно писати іменники (noun), займенники (pronoun), дієслова(verb), прикметники(adjective), прислівники(adverb) та підрядні сполучники.
-        :param text:
-        :return:
-        '''
-
-        modified_text = ""
-
-        doc = pureHeader.nlp(text)
-
-        for token in doc:
             is_capitalize = False
-
-            if token.tag_ == "NOUN": is_capitalize = True
-            if token.tag_ == "PRON": is_capitalize = True
-            if token.tag_ == "VB": is_capitalize = True
-            if token.tag_ == "ADJ": is_capitalize = True
-            if token.tag_ == "ADV": is_capitalize = True
-            if token.tag_ == "SCONJ": is_capitalize = True
-
-            if is_capitalize == True:
-                modified_text += token.text.capitalize() + token.whitespace_
-            else:
-                modified_text += token.text + token.whitespace_
-
-        return modified_text
-
-
-    def ruleForDash(self, text):
-        '''
-        Якщо слово написане через дефіс, велику літеру потрібно додати для кожної частинки слова (наприклад, правильно "Self-Reflection", а не "Self-reflection").
-        :param text:
-        :return:
-        '''
-
-        is_capitalize = False
-        modified_text = ""
-
-        doc = pureHeader.nlp(text)
-
-        for i, token in enumerate(doc):
-            is_capitalize = False
-
-            if i > 0 and i < len(doc) - 1:
-                is_capitalize = True if doc[i + 1].text == '-' else 0
-                is_capitalize = True if doc[i - 1].text == '-' else 0
-
-            if is_capitalize == True:
-                modified_text += token.text.capitalize() + token.whitespace_
-            else:
-                modified_text += token.text + token.whitespace_
-
-        return modified_text
-
-
-    def ruleForOther(self, text):
-        '''
-        З маленької літери потрібно писати всі інші частини мови: артиклі/визначники (DET), сурядні сполучники(CONJ, CCONJ), прийменники(ADP), частки(PART), вигуки(INTJ).
-        :param text:
-        :return:
-        '''
-
-        is_lowercased = False
-        modified_text = ""
-        doc = pureHeader.nlp(text)
-
-        for token in doc:
             is_lowercased = False
 
-            if token.tag_ == "DET": is_lowercased = True
-            if token.tag_ == "CONJ":  is_lowercased = True
-            if token.tag_ == "CCONJ": is_lowercased = True
-            if token.tag_ == "ADP": is_lowercased = True
-            if token.tag_ == "PART": is_lowercased = True
-            if token.tag_ == "INTJ": is_lowercased = True
-            if token.tag_ == "TO": is_lowercased = True
-            #if token.tag_ == "DT": is_lowercased = True
-
-            if is_lowercased == True:
-                modified_text += token.text.lower() + token.whitespace_
-            else:
+            # rule_fourLonger check
+            if len(token) > 3:
+                is_capitalize = True
+            # check if NER dont change word
+            if token.ent_type_ == 'ORG':
                 modified_text += token.text + token.whitespace_
+                continue
+
+            # chek if word have couple of cappital letters
+
+            if len(re.findall(r'[A-Z]',token.text))>1:
+                modified_text += token.text + token.whitespace_
+                continue
+
+            #rule_rule_capFirstLast check
+            if i == len(doc) - 1 or i == 0:
+                is_capitalize = True
+
+            if i == 1:
+                if doc[0].text == '\'':
+                    modified_text += token.text.capitalize() + token.whitespace_
+                    continue
+            # rule_forLangParts check
+            if token.pos_ == "NOUN": is_capitalize = True
+            if token.pos_ == "PRON": is_capitalize = True
+            if token.pos_ == "VERB": is_capitalize = True
+            if token.pos_ == "ADJ": is_capitalize = True
+            if token.pos_ == "ADV": is_capitalize = True
+            if token.pos_ == "SCONJ" and token.dep_ != 'prep' : is_capitalize = True
+            if token.pos_ == "NUM" : is_capitalize = True
+
+            if token.shape_ == "'x":
+                modified_text += token.text + token.whitespace_
+                continue
+
+            if token.lemma_ == 'be':
+                modified_text += token.text.capitalize() + token.whitespace_
+                continue
+            # rule fo 'Is' -
+            if token.pos_ == "AUX":
+                t=1
+
+
+            # rule for ''s' - case
+            if token.pos_ == "AUX" and token.dep_ == 'case':
+                modified_text += token.text + token.whitespace_
+                continue
+
+            # rule_forDash check
+            if i > 0 and i < len(doc) - 1:
+                if doc[i + 1].text == '-': is_capitalize = True
+                if doc[i - 1].text == '-': is_capitalize = True
+
+            # rule_forOther check
+            if token.pos_ == "DET": is_lowercased = True
+            if token.pos_ == "CONJ":  is_lowercased = True
+            if token.pos_ == "CCONJ": is_lowercased = True
+            if token.pos_ == "ADP": is_lowercased = True
+
+            if token.pos_ == "PART":
+                if token.text == "not" or token.text == "Not":
+                    modified_text += token.text.capitalize() + token.whitespace_
+                    continue
+                else:
+                    is_lowercased = True
+
+
+            if token.pos_ == "INTJ": is_lowercased = True
+
+            # применение правил
+            if is_capitalize == True:
+                if token.text.isupper() == True:
+                    modified_text += token.text + token.whitespace_
+                else:
+                    modified_text += token.text.capitalize() + token.whitespace_
+            else:
+                if is_lowercased == True:
+                    modified_text += token.text.lower() + token.whitespace_
+                else:
+                    modified_text += token.text + token.whitespace_
 
         return modified_text
 
@@ -150,7 +121,7 @@ def accuracy(file):
     with open(file) as json_file:
         data = json.load(json_file)
 
-    for d in tqdm(data):
+    for i, d in tqdm(enumerate(data)):
         h = pureHeader(d[0])
         if h.modified_text == d[1]:
             correct_headers += 1
@@ -170,8 +141,6 @@ with open('../tasks/02-structural-linguistics/data/headlines-test-set.json') as 
     data = json.load(json_file)
 
 for i, d in enumerate(data):
-    if i == 1:
-        t = 1
     h = pureHeader(d[0])
     if h.modified_text == d[1]:
         correct_headers += 1
