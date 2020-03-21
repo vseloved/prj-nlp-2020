3f215c6f61e02c42ab2a52592c7bf827110295e0
holdbar <holdbarhet@gmail.com>
2020-03-21 19:20:26 +0200

    add rest of hw

--- students/KostiantynZuiev/02-structural-linguistics/en_collocations.py
+++ students/KostiantynZuiev/02-structural-linguistics/en_collocations.py
@@ -21,7 +21,6 @@ def collect_stat(text, result_dict):
     for token in doc:
         if (token.pos_ == "VERB" or token.tag_ in ["VB", "VBD", "VBG", "VBN", "VBP", "VBZ"])\
             and (token.text in verbs or token.lemma_ in verbs):
-            # print("???? parent verb:", token)
             parent_verb = token
             if result_dict.get(parent_verb.lemma_) is None:
                 result_dict[parent_verb.lemma_] = {}
@@ -32,16 +31,31 @@ def collect_stat(text, result_dict):
                     else:
                         result_dict[parent_verb.lemma_][t.text] += 1
 
-                    # print("######")
-                    # print(doc)
-                    # print("$$$HEAD: ",t.head)
     return result_dict
 
-            
+def format_result(result_dict):
+    results = []
+    for verb, adverbs in result_dict.items():
+        sorted_adverbs = sorted(list(adverbs.items()),key=lambda x:(-x[1],x[0]))
+        result_string = f'{verb}: '
+        for adv_tuple in sorted_adverbs[:10]:
+            result_string += f'{adv_tuple}, '
+        
+        results.append(result_string.strip(', '))
+
+    return results
+
+
 
 if __name__ == "__main__":
     with open('data/blog2008.txt','r') as f:
         result_dict = {}
+        i = 0
         for line in f:
             result_dict = collect_stat(line, result_dict)
-        print(result_dict)
+            i += 1
+            print(i)
+        results = format_result(result_dict)
+        with open('en_collocations_results.txt', 'w') as res_file:
+            for result in results:
+                res_file.write(f'{result}\n')
