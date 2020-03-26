9e99b3da00183488440dd30a8313ab2752f6e109
b <yatsinaba@gmail.com>
2020-03-26 17:46:43 +0200

    виправлення зауважень Маряни https://github.com/vseloved/prj-nlp-2020/pull/33#discussion_r398049316 https://github.com/vseloved/prj-nlp-2020/pull/33#discussion_r398050175 https://github.com/vseloved/prj-nlp-2020/pull/33#discussion_r398053698 https://github.com/vseloved/prj-nlp-2020/pull/33#discussion_r398054208 https://github.com/vseloved/prj-nlp-2020/pull/33#discussion_r398058079 https://github.com/vseloved/prj-nlp-2020/pull/33#discussion_r398058962 https://github.com/vseloved/prj-nlp-2020/pull/33#discussion_r398060221 https://github.com/vseloved/prj-nlp-2020/pull/33#discussion_r398064295 https://github.com/vseloved/prj-nlp-2020/pull/33#discussion_r398065397

--- students/BohdanYatsyna/homework2/task-02-3-2.py
+++ students/BohdanYatsyna/homework2/task-02-3-2.py
@@ -10,7 +10,6 @@ nlp = stanza.Pipeline('uk')
 with open("../tasks/02-structural-linguistics/data/tyhrolovy.txt") as file:
     data = file.readlines()
 
-
 cleaned_data = []
 word_bigram = {}
 for i, d in enumerate(data):
@@ -22,18 +21,15 @@ for i, d in enumerate(data):
 for d in tqdm(cleaned_data):
     doc = nlp(d)
     for sentence in doc.sentences:
-        for noun_i, word in enumerate(sentence.words):
-            if word.text.lower() == "старий":
-                t = 1
-            if word.pos == "PROPN" or word.upos == "NOUN":
-                if 'Animacy=Anim' in word.feats:
-                    for w in sentence.words:
-                        if w.upos == "ADJ":
-                            if w.head == noun_i + 1:
-                                try:
-                                    word_bigram[word.text.lower() + ' ' + w.text.lower()] += 1
-                                except:
-                                    word_bigram.update({word.text.lower() + ' ' + w.text.lower(): 1})
+        for i, word in enumerate(sentence.words):
+            if word.upos == "ADJ":
+                if sentence.words[word.head - 1].upos in ["PROPN", "NOUN"] \
+                        and 'Animacy=Anim' in sentence.words[word.head - 1].feats:
+
+                    try:
+                        word_bigram[sentence.words[word.head - 1].lemma + ' ' + word.lemma] += 1
+                    except:
+                        word_bigram.update({sentence.words[word.head - 1].lemma + ' ' + word.lemma: 1})
 
 word_bigram = dict(OrderedDict(sorted(word_bigram.items(), key=itemgetter(0))))
 word_bigram = dict(OrderedDict(sorted(word_bigram.items(), key=itemgetter(1), reverse=True)))
--- students/BohdanYatsyna/homework2/task-02-3.py
+++ students/BohdanYatsyna/homework2/task-02-3.py
@@ -2,6 +2,7 @@ import spacy
 from tqdm import tqdm
 from collections import OrderedDict
 from operator import itemgetter
+import re
 
 nlp = spacy.load("en_core_web_md")
 
@@ -11,26 +12,28 @@ with open("../tasks/02-structural-linguistics/data/blog2008.txt") as file:
 say = ["say", "tell", "speak", "claim", "communicate", "add", "announce", "answer", "assert", "articulate", "convey",
        "declare", "deliver", "disclose", "do", "enunciate", "estimate", "express", \
        "maintain", "mention", "order", "pronounce", "read", "repeat", "reply", "report", "respond", "reveal", "state",
-       "suggest", "suppose", "voice", \
-       "talk", "verbalize", "vocalize"]
-count = 0
+       "suggest", "suppose", "voice", "talk", "verbalize", "vocalize"]
+
 word_pair_freq = {}
 
 for d in tqdm(data):
+    # check that string contains a word with -ly end
+    if len(re.findall(r'\w*ly\b', d)) == 0:
+        continue
+
     doc = nlp(d)
     for token in doc:
-        if token.pos == 86 and token.text[-2:] == "ly" and \
-                token.head.pos == 100 and token.head.text in say:
-            count += 1
+        if token.pos_ == 'ADV' and token.text[-2:] == "ly" and \
+                token.head.pos_ == 'VERB' and token.head.lemma_ in say:
 
             try:
-                word_pair_freq[token.head.text.lower()][token.text.lower()] += 1
+                word_pair_freq[token.head.lemma_][token.text.lower()] += 1
             except KeyError:
 
-                if word_pair_freq.get(token.head.text.lower(), False):
-                    word_pair_freq[token.head.text.lower()].update({token.text.lower(): 1})
+                if word_pair_freq.get(token.head.lemma_, False):
+                    word_pair_freq[token.head.lemma_].update({token.text.lower(): 1})
                 else:
-                    word_pair_freq[token.head.text.lower()] = {token.text.lower(): 1}
+                    word_pair_freq[token.head.lemma_] = {token.text.lower(): 1}
 
 word_pair_freq = dict(OrderedDict(sorted(word_pair_freq.items(), key=itemgetter(0))))
 
@@ -39,4 +42,4 @@ for key, value in word_pair_freq.items():
 
 with open('../students/BohdanYatsyna/homework2/task-02-3-statistic.txt', 'w') as f:
     for key, value in word_pair_freq.items():
-        print('{}: {}'.format(key, value),file=f)
+        print('{}: {}'.format(key, value), file=f)
