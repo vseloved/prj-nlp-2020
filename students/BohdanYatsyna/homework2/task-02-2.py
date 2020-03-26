4740d25f7a81c1e6f1e499ce849131136a2f9da7
b <yatsinaba@gmail.com>
2020-03-26 17:46:43 +0200

    виправлення зауважень Маряни https://github.com/vseloved/prj-nlp-2020/pull/33#discussion_r398049316 https://github.com/vseloved/prj-nlp-2020/pull/33#discussion_r398050175 https://github.com/vseloved/prj-nlp-2020/pull/33#discussion_r398053698 https://github.com/vseloved/prj-nlp-2020/pull/33#discussion_r398054208 https://github.com/vseloved/prj-nlp-2020/pull/33#discussion_r398058079 https://github.com/vseloved/prj-nlp-2020/pull/33#discussion_r398058962 https://github.com/vseloved/prj-nlp-2020/pull/33#discussion_r398060221 https://github.com/vseloved/prj-nlp-2020/pull/33#discussion_r398064295 https://github.com/vseloved/prj-nlp-2020/pull/33#discussion_r398065397

--- students/BohdanYatsyna/homework2/task-02-2.py
+++ students/BohdanYatsyna/homework2/task-02-2.py
@@ -25,20 +25,29 @@ text_with_pos = ""
 
 with open("../tasks/02-structural-linguistics/data/examiner-headlines.txt") as file:
     data = file.readlines()
-statistic = {'entity':0, 'most lvl':0, 'polarity':0}
+statistic = {'entity':0, 'more lvl':0, 'most lvl':0, 'polarity':0}
 for line in tqdm(data):
     have_entenies = False
+    have_more_lvl = False
     have_most_lvl = False
     have_polarity = False
     doc = nlp(line)
     for token in doc:
         # enteties presence in token
-        if token.ent_type_ == "PESRON" or token.ent_type_ == "ORG" or token.ent_type_ == "GPE"\
+        if token.ent_type_ == "PERSON" or token.ent_type_ == "ORG" or token.ent_type_ == "GPE"\
         or token.ent_type_ == "FAC" or token.ent_type_ == "EVENT" or token.ent_type_ == "PRODUCT":
             have_entenies = True
 
         # super ADV and ADG
         if token.pos_ == "ADJ" or token.pos_ == "ADV":
+            # more lvl check
+            if token.text[-2:] == "er":
+                have_more_lvl = True
+            if token.text.lower() == "worse" or token.text.lower() == "more" or token.text.lower() == "less":
+                have_more_lvl = True
+
+
+            # most lvl check
             if token.text[-3:] == "est":
                 have_most_lvl = True
             if token.text.lower() == "worst" or token.text.lower() == "most" or token.text.lower() == "least":
@@ -50,9 +59,10 @@ for line in tqdm(data):
         have_polarity = True
 
     if have_entenies: statistic['entity'] += 1
+    if have_more_lvl: statistic['more lvl'] += 1
     if have_most_lvl: statistic['most lvl'] += 1
     if have_polarity: statistic['polarity'] += 1
 
 with open('../students/BohdanYatsyna/homework2/task-02-2-statistic.txt', 'w') as f:
-    print('examiner-headlines.txt - statistic entities: {:2.2%} most lvl: {:2.2%} polarity: {:2.2%}'.format(statistic['entity']/5000,\
-            statistic['most lvl']/5000,statistic['polarity']/5000),file=f)
+    print('examiner-headlines.txt - statistic entities: {:2.2%}, more lvl:{:2.2%}, most lvl: {:2.2%}, polarity: {:2.2%}'.\
+          format(statistic['entity']/5000, statistic['more lvl']/5000,statistic['most lvl']/5000,statistic['polarity']/5000),file=f)
