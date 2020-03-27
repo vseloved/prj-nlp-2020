2930ce97718d1ddbc0450a14658e4c4929ae5f05
b <yatsinaba@gmail.com>
2020-03-27 17:49:16 +0200

    прото SAX парсер

---
+++ students/BohdanYatsyna/task-03-SAX.py
@@ -0,0 +1,48 @@
+import xml.sax
+import subprocess
+
+class WikiXmlHandler(xml.sax.handler.ContentHandler):
+    """Content handler for Wiki XML data using SAX"""
+    def __init__(self):
+        xml.sax.handler.ContentHandler.__init__(self)
+        self._buffer = None
+        self._values = {}
+        self._current_tag = None
+        self._pages = []
+
+    def characters(self, content):
+        """Characters between opening and closing tags"""
+        if self._current_tag:
+            self._buffer.append(content)
+
+    def startElement(self, name, attrs):
+        """Opening tag of element"""
+        if name in ('title', 'text'):
+            self._current_tag = name
+            self._buffer = []
+
+    def endElement(self, name):
+        """Closing tag of element"""
+        if name == self._current_tag:
+            self._values[name] = ' '.join(self._buffer)
+
+        if name == 'page':
+            self._pages.append((self._values['title'], self._values['text']))
+
+# Object for handling xml
+handler = WikiXmlHandler()
+# Parsing object
+parser = xml.sax.make_parser()
+parser.setContentHandler(handler)
+# Iteratively process file
+for line in subprocess.Popen(['bzcat'],
+                             stdin = open(data_path),
+                             stdout = subprocess.PIPE).stdout:
+    parser.feed(line)
+    parser.feed()
+    # Stop when 3 articles have been found
+    if len(handler._pages) > 2:
+        break
+
+    #parser.parse("/Users/bogdan/Downloads/dewiki-20200301-pages-articles-multistream.xml")
+    #t = 1
