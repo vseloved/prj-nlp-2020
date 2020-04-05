71d7d8640fc23c69dc815cf8075cbc103918ff56
Liss <alicedeadbride@gmail.com>
2020-04-05 23:54:58 +0300

    The very draft of hw 5 beginning

--- students/AlisaMansurova/03-data/crowley/types.ts
+++ students/AlisaMansurova/03-data/crowley/types.ts
@@ -1,6 +1,13 @@
 export interface Article {
-    title: string;
-    date: string;
-    content: string;
-    tags?: string[];
+    title: string | null;
+    date: string | null;
+    content: string | null;
+    tags?: (string | null)[];
+}
+
+export interface Review {
+    rating: number | null;
+    text?: string;
+    pros?: string;
+    cons?: string;
 }
