5840498aebb17563774848379e0f44bb807552a4
b <yatsinaba@gmail.com>
2020-05-06 20:18:31 +0300

    перше наближення

--- students/BohdanYatsyna/homework6/prediction.py
+++ students/BohdanYatsyna/homework6/prediction.py
@@ -9,31 +9,13 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.metrics import plot_confusion_matrix
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import numpy as np
 
-from sklearn.linear_model import SGDClassifier
 from time import time
 from datetime import datetime
-import spacy
-### Фічі для моделі
-def feachure_extractor(sent):
-    feachure_sent = []
-    #doc = nlp(' '.join(sent))
-    doc = sent
 
-    for i, word in enumerate(doc):
-        features = dict()
-        #features['word'] = word
-        features["word-1"] = doc[i-1] if i > 0 else "NONE"
-        features['is title'] = word.istitle()
-        features['start'] = True if not i else False
-        features['end'] = True if i == len(doc) - 1 else False
-        #features['is_upper'] = word.isupper()
-        #features["parent"] = sentence[i].dep_ + "_" + sentence[i].head.lemma_
-        #features["right-bigram"] = sentence[i+1].text + "_" + sentence[i+2].text if i < (len(sentence) - 2) else "NONE"
-        feachure_sent.append(features)
-    return feachure_sent
 
-def benchmark(X_train, X_val, X_test, y_train, y_val,y_test, pipeline, parameters, label=''):
+def benchmark(X_train, X_val, X_test, y_train, y_val, y_test, pipeline, parameters, label=''):
     now = datetime.now()
     date_time = now.strftime("%m-%d-%Y__%H-%M-%S")
     with open('./iterations/' + pipeline[1].__class__.__name__ + '-' + label + '--' + date_time, 'w+') as f:
@@ -81,83 +63,61 @@ def benchmark(X_train, X_val, X_test, y_train, y_val,y_test, pipeline, parameter
                           ("Normalized confusion matrix", 'true')]
         for title, normalize in titles_options:
             disp = plot_confusion_matrix(grid_search, X_test, y_test,
-                                 cmap=plt.cm.Blues,
-                                 normalize=normalize)
+                                         cmap=plt.cm.Blues,
+                                         normalize=normalize)
         disp.ax_.set_title(title)
 
         print(title, file=f)
-        print(disp.confusion_matrix,file=f)
-
+        print(disp.confusion_matrix, file=f)
         plt.show()
 
+        """Prints features with the highest coefficient values, per class"""
+        feature_names = vec.get_feature_names()
+        for i, class_label in enumerate(grid_search.classes_):
+            top10 = np.argsort(grid_search.coef_[i])[-10:]
+            print("%s: %s" % (class_label,
+                              " ".join(feature_names[j] for j in top10)))
 
-nlp = spacy.load("en_core_web_sm")
+print("Load dataset....")
 ### загрузка данних
 DATASET_FILE = 'dataset.json'
 with open(DATASET_FILE) as file:
     dataset = json.load(file)
 
-### підготовка дев датасету
-print("Load DEV dataset....")
-feachures_dataset = []
-labels_dataset = []
-for sent, labels in tqdm(zip(dataset['dev']['tokens'], dataset['dev']['labels'])):
-    feachures_dataset.extend(feachure_extractor(sent))
-    labels_dataset.extend(labels)
-
-
 ### Векторизація дев сету
 vec = DictVectorizer()
-vec = vec.fit(feachures_dataset)
-#print("Total number of features: {}\nFeature list: {}".format(len(vec.get_feature_names()), vec.get_feature_names()))
+vec = vec.fit(dataset['dev']['tokens'])
+# print("Total number of features: {}\nFeature list: {}".format(len(vec.get_feature_names()), vec.get_feature_names()))
 
-features_vectorized = vec.transform(feachures_dataset)
-
+features_vectorized = vec.transform(dataset['dev']['tokens'])
+labels_dataset = dataset['dev']['labels']
 # спліт  на дев та валідаційний
 X_train, X_val, y_train, y_val = train_test_split(features_vectorized, labels_dataset, test_size=0.33, random_state=42)
 
-### Підготовка тестового датасету
-print("Load TEST dataset....")
-test_feachures_dataset = []
-test_labels_dataset = []
-for sent, labels in tqdm(zip(dataset['test']['tokens'], dataset['test']['labels'])):
-    test_feachures_dataset.extend(feachure_extractor(sent))
-    test_labels_dataset.extend(labels)
-
-X_test = vec.transform(test_feachures_dataset)
-y_test = test_labels_dataset
-
-#lrc = LogisticRegression(random_state=42, solver="sag", multi_class="multinomial", max_iter=1000, verbose=1)
-
-# lrc.fit(X_train, y_train)
-#
-# pred1 = lrc.predict(X_val)
-# print(classification_report(y_val, pred1))
-
-features_vectorized = vec.transform(feachures_dataset)
+X_test = vec.transform(dataset['test']['tokens'])
+y_test = dataset['test']['labels']
 
 X_train, X_val, y_train, y_val = train_test_split(features_vectorized, labels_dataset, test_size=0.33, random_state=42)
 
 parameters = {
-    #'vect__max_df': (0.5, 0.75, 1.0),
-    #'vect__max_features': (None, 500, 1000, 2000, 5000),
-    #'vect__ngram_range': ((1, 1), (1, 2), (1, 3)),  # unigrams or bigrams or thrigrams
     'tfidf__use_idf': (True, False),
     'tfidf__norm': ('l1', 'l2'),
     'clf__tol': (0.0001,),
     'clf__solver': ('sag',),
-    'clf__penalty': ('l2', ),
+    'clf__penalty': ('l2',),
     'clf__max_iter': (1000,),
     'clf__random_state': (42,)
 
 }
 # Побачити параметри для любого елемента пайплайну
-#print(pipeline['clf'].get_params().keys())
+# print(pipeline['clf'].get_params().keys())
 
 pipeline = Pipeline([
-    #('vect', DictVectorizer()),
     ('tfidf', TfidfTransformer()),
     ('clf', LogisticRegression()),
 ])
 
-benchmark(X_train, X_val,X_test, y_train, y_val, y_test, pipeline, parameters)
+grid_search = benchmark(X_train, X_val, X_test, y_train, y_val, y_test, pipeline, parameters)
+
+
+#print_top10(vec,pipeline['clf'],pipeline['clf'].classes_)
--- students/BohdanYatsyna/homework6/preprocess1.py
+++ students/BohdanYatsyna/homework6/preprocess1.py
@@ -4,7 +4,84 @@ from random import seed
 from random import randint
 import spacy
 import en_core_web_md
-from tqdm import tqdm
+from spacy.tokens import Doc
+from tqdm import tqdm, trange
+import numpy as np
+import re
+
+def shape(word):
+    if re.match('[0-9]+(\.[0-9]*)?|[0-9]*\.[0-9]+$', word):
+        return 'number'
+    elif re.match('\W+$', word):
+        return 'punct'
+    elif re.match('[A-Z][a-z]+$', word):
+        return 'capitalized'
+    elif re.match('[A-Z]+$', word):
+        return 'uppercase'
+    elif re.match('[a-z]+$', word):
+        return 'lowercase'
+    elif re.match('[A-Z][a-z]+[A-Z][a-z]+[A-Za-z]*$', word):
+        return 'camelcase'
+    elif re.match('[A-Za-z]+$', word):
+        return 'mixedcase'
+    elif re.match('__.+__$', word):
+        return 'wildcard'
+    elif re.match('[A-Za-z0-9]+\.$', word):
+        return 'ending-dot'
+    elif re.match('[A-Za-z0-9]+\.[A-Za-z0-9\.]+\.$', word):
+        return 'abbreviation'
+    elif re.match('[A-Za-z0-9]+\-[A-Za-z0-9\-]+.*$', word):
+        return 'contains-hyphen'
+    return 'other'
+
+def get_rand_sent(buffer):
+    if len(buffer) > 4:
+        return randint(1, 4)
+    else:
+        return randint(1, len(buffer))
+
+def feachure_extractor(sent):
+    feachure_sent = []
+    doc = sent
+
+    for i, word in enumerate(doc):
+        features = dict()
+        features['lema'] = word.lemma_
+        features['pos'] = word.pos_
+        features["word-1"] = doc[i-1].text if i > 0 else "NONE"
+        features["word-2"] = doc[i-1].text if i > 0 else "NONE"
+
+        features['shape'] = shape(word.text)
+        features["parent"] = doc[i].dep_ + "_" + doc[i].head.lemma_
+        features["right-bigram"] = doc[i+1].text + "_" + doc[i+2].text if i < (len(doc) - 2) else "NONE"
+        feachure_sent.append(features)
+    return feachure_sent
+
+def generate_dataset_string(buff_list):
+    gen_tokens_labels = {}
+
+    for item in buff_list:
+        doc = nlp(item)
+
+        first_cap = randint(0,1)
+        miss_point =randint(0,1)
+        gen_tokens_labels['tokens'] = []
+        gen_tokens_labels['labels'] = []
+        extracted_features = feachure_extractor(doc)
+
+        for i in range(len(doc)):
+
+            if not i and not first_cap:
+                gen_tokens_labels['tokens'].append(extracted_features[i])
+                gen_tokens_labels['labels'].append(False)
+            elif i== len(doc) -1 and doc[i].pos_ == 'PUNCT' and miss_point:
+                gen_tokens_labels['labels'][-1] = True
+                t=1
+            else:
+                gen_tokens_labels['tokens'].append(extracted_features[i])
+                gen_tokens_labels['labels'].append(False)
+
+    return gen_tokens_labels
 
 nlp = spacy.load("en_core_web_md")
 RAW_SENTENCES_FILE = '../stripped_masc_sentences.json'
@@ -14,65 +91,46 @@ DATASET_FILE = './dataset.json'
 joined_sent_tokens = []
 joined_sent_labels = []
 dataset = {}
+
+print('Load Raw data...')
 with open(RAW_SENTENCES_FILE) as json_file:
     data = json.load(json_file)
 
 dataset['dev'] = {'tokens': [],'labels': []}
 dataset['test']= {'tokens': [],'labels': []}
 
+print('Prepearing TEST dataset part...')
 with open(TEST_DATASET) as json_file:
     tt = json.load(json_file)
-    for t in tt:
+    for t in tqdm(tt,desc= "Loading test tokens and labels"):
         temp_sent = []
         temp_label = []
         for item in t:
             temp_sent.append(item[0])
             temp_label.append(item[1])
-        dataset['test']['tokens'].append(temp_sent)
-        dataset['test']['labels'].append(temp_label)
+        #doc_test = nlp(' '.join(temp_sent))
+        doc_test = Doc(nlp.vocab, words=temp_sent)
+        dataset['test']['tokens'].extend(feachure_extractor(doc_test))
+        dataset['test']['labels'].extend(temp_label)
 
 seed(40)
 
-
-def get_rand_sent(buf):
-    if len(buffer) > 4:
-        return randint(1, 4)
-    else:
-        return randint(1, len(buffer))
-
-def generate_dataset_string(buff_list):
-    print('len {}   {}'.format(len(buff_list),buff_list))
-    gen_tokens_labels = {}
-
-    for item in buff_list:
-        doc = nlp(item)
-        first_cap = randint(0,1)
-        miss_point =randint(0,1)
-        gen_tokens_labels['tokens'] = []
-        gen_tokens_labels['labels'] = []
-
-        for i,token in enumerate(doc):
-            if not i and not first_cap:
-                gen_tokens_labels['tokens'].append(token.text.lower())
-                gen_tokens_labels['labels'].append(False)
-            elif i== len(doc) -1 and token.pos_ == 'PUNCT' and miss_point:
-                gen_tokens_labels['labels'][-1] = True
-            else:
-                gen_tokens_labels['tokens'].append(token.text)
-                gen_tokens_labels['labels'].append(False)
-
-    return gen_tokens_labels
-
 buffer = data
-while tqdm(buffer):
-    num_sent = get_rand_sent(buffer)
-    joined_sent_tokens_labels = generate_dataset_string(buffer[:num_sent])
-    del buffer[:num_sent]
-    dataset['dev']['tokens'].append(joined_sent_tokens_labels['tokens'])
-    dataset['dev']['labels'].append(joined_sent_tokens_labels['labels'])
 
+with tqdm(total=len(buffer), desc="Prepearing DEV dataset tokens and labels") as pbar:
+    while buffer:
+        num_sent = get_rand_sent(buffer)
+        joined_sent_tokens_labels = generate_dataset_string(buffer[:num_sent])
+        del buffer[:num_sent]
+        dataset['dev']['tokens'].extend(joined_sent_tokens_labels['tokens'])
+        dataset['dev']['labels'].extend(joined_sent_tokens_labels['labels'])
+        pbar.update(num_sent)
+
+print('Write to file dataset...')
 with open(DATASET_FILE,'w') as file:
     json.dump(dataset, file)
+print('DONE')
+
 
 
 
