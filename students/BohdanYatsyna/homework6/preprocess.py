1c90c387311501ad14338aad4709b1740bdc6496
b <yatsinaba@gmail.com>
2020-05-24 13:10:20 +0300

    почистив 6-1

--- students/BohdanYatsyna/homework6/preprocess.py
+++ students/BohdanYatsyna/homework6/preprocess.py
@@ -1,6 +1,8 @@
 import csv
 import json
+
 from tqdm import tqdm
+
 RAW_DATA_FILE = './masc/masc_sentences.tsv'
 RAW_SENTENCES_FILE = '../stripped_masc_sentences.json'
 
@@ -14,7 +16,6 @@ def read_raw_data():
 
     with open(RAW_SENTENCES_FILE, 'w') as out_file:
         json.dump(data, out_file)
-
 
 if __name__ == "__main__":
     read_raw_data()
