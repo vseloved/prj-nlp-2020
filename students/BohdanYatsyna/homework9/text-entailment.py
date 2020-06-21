1a6ccb8049f8382f0a90606a1cbad8f8539bd275
b <yatsinaba@gmail.com>
2020-06-21 19:29:19 +0300

    added normalizaion
    normalization produce worse results for now
    
    total acc = 0.55

--- students/BohdanYatsyna/homework9/text-entailment.py
+++ students/BohdanYatsyna/homework9/text-entailment.py
@@ -47,7 +47,7 @@ def get_model(model_name):
         clf_model = GaussianNB()
 
     if model_name == 'gbc':
-        clf_model = GradientBoostingClassifier(verbose=1)
+        clf_model = GradientBoostingClassifier(verbose=1, tol=0.00001, n_estimators=200)
 
     if model_name == 'adaboost':
         clf_model = AdaBoostClassifier(n_estimators=500, learning_rate=0.01, random_state=42,
@@ -59,7 +59,7 @@ def get_model(model_name):
     return clf_model
 
 
-def train_model(cls, X_train, y_train, X_test, y_test, name, wandb):
+def train_model(cls, wandb):
 
     if wandb.config.norm:
         clf = Pipeline([
@@ -73,13 +73,34 @@ def train_model(cls, X_train, y_train, X_test, y_test, name, wandb):
             ('cls', cls)
         ])
     clf.fit(train_features, train_labels)
-    wandb_classification_report(clf, X_train, X_test, y_train, y_test, name, wandb)
+    return clf
 
 
 config_defaults = {
     'solver': 'sag',
     'model': 'gbc',
-    'norm': False
+    'norm': False,
+    'f_lemma': 1,
+    'f_lemma_verb': 1,
+    'f_lemma_noun': 1,
+    'f_lemma_adj': 1,
+    'f_lemma_adv': 1,
+    'f_noun_prases': 1,
+    'f_ner': 1,
+    'f_bleu_avg': 1,
+    'f_bleu_1': 1,
+    'f_bleu_2': 1,
+    'f_bleu_3': 1,
+    'f_bleu_4': 1,
+    'f_rouge1': 1,
+    'f_rouge2': 1,
+    'f_rougel': 1,
+    'f_wer': 1,
+    'f_sim_lema': 1,
+    'f_sim_verb': 1,
+    'f_len_sent1': 1,
+    'f_len_sent2': 1,
+    'f_syn': 1,
 }
 if __name__ == '__main__':
 
@@ -89,15 +110,40 @@ if __name__ == '__main__':
     parser.add_argument('--dev_file', help='Train file location ', default="snli_1.0/snli_1.0_dev.jsonl")
     parser.add_argument('--test_file', help='Train file location ', default="snli_1.0/snli_1.0_test.jsonl")
     parser.add_argument('--proba', help='If True - app work as sample with cutted data', default=False)
-    parser.add_argument('--n_jobs', help='Number of workers (default=1) use all cores', default=4)
+    parser.add_argument('--n_jobs', help='Number of workers (default=1) use all cores', default=8)
     parser.add_argument('--solver', help='type of solver for model (default=sag)', default='sag')
     parser.add_argument('--model', help='model type', default='gbc')
     parser.add_argument('--norm', help='Normalizer On/Off (default=False)', default=False)
+    # features
+    parser.add_argument('--f_lemma', help='f_lemma', default=1)
+    parser.add_argument('--f_lemma_verb', help='f_lemma_verb', default=1)
+    parser.add_argument('--f_lemma_noun', help='f_lemma_noun', default=1)
+    parser.add_argument('--f_lemma_adj', help='f_lemma_adj', default=1)
+    parser.add_argument('--f_lemma_adv', help='f_lemma_adv', default=1)
+    parser.add_argument('--f_noun_prases', help='f_noun_prases', default=1)
+    parser.add_argument('--f_ner', help='f_ner', default=1)
+    parser.add_argument('--f_bleu_avg', help='f_bleu_avg', default=1)
+    parser.add_argument('--f_bleu_1', help='f_bleu_1', default=1)
+    parser.add_argument('--f_bleu_2', help='f_bleu_2', default=1)
+    parser.add_argument('--f_bleu_3', help='f_bleu_3', default=1)
+    parser.add_argument('--f_bleu_4', help='f_bleu_4', default=1)
+    parser.add_argument('--f_rouge1', help='f_rouge1', default=1)
+    parser.add_argument('--f_rouge2', help='f_rouge2', default=1)
+    parser.add_argument('--f_rougel', help='f_rougel', default=1)
+    parser.add_argument('--f_wer', help='f_wer', default=1)
+    parser.add_argument('--f_sim_lema', help='f_sim_lema', default=1)
+    parser.add_argument('--f_sim_verb', help='f_sim_verb', default=1)
+    parser.add_argument('--f_len_sent1', help='f_len_sent1', default=1)
+    parser.add_argument('--f_len_sent2', help='f_len_sent2', default=1)
+    parser.add_argument('--f_syn', help='f_syn', default=1)
+
+
+
 
     spacy.prefer_gpu()
 
     Token.set_extension("is_negative", default=False)
-    nlp = spacy.load("en_core_web_md", disable=['ner'])
+    nlp = spacy.load("en_core_web_md")
     nlp.add_pipe(add_negation)
     print(nlp.pipe_names)
 
@@ -107,6 +153,28 @@ if __name__ == '__main__':
     wandb.config.solver = opts.solver
     wandb.config.model = opts.model
     wandb.compat.norm = opts.norm
+    # features
+    wandb.config.f_lemma = opts.f_lemma
+    wandb.config.f_lemma_verb = opts.f_lemma_verb
+    wandb.config.f_lemma_noun = opts.f_lemma_noun
+    wandb.config.f_lemma_adj = opts.f_lemma_adj
+    wandb.config.f_lemma_adv = opts.f_lemma_adv
+    wandb.config.f_noun_prases = opts.f_noun_prases
+    wandb.config.f_ner = opts.f_ner
+    wandb.config.f_bleu_avg = opts.f_bleu_avg
+    wandb.config.f_bleu_1 = opts.f_bleu_1
+    wandb.config.f_bleu_2 = opts.f_bleu_2
+    wandb.config.f_bleu_3 = opts.f_bleu_3
+    wandb.config.f_bleu_4 = opts.f_bleu_4
+    wandb.config.f_rouge1 = opts.f_rouge1
+    wandb.config.f_rouge2 = opts.f_rouge2
+    wandb.config.f_rougel = opts.f_rougel
+    wandb.config.f_wer = opts.f_wer
+    wandb.config.f_sim_lema = opts.f_sim_lema
+    wandb.config.f_sim_verb = opts.f_sim_verb
+    wandb.config.f_len_sent1 = opts.f_len_sent1
+    wandb.config.f_len_sent2 = opts.f_len_sent2
+    wandb.config.f_syn = opts.f_syn
 
     # measure execution time
     start_time = datetime.now()
@@ -125,18 +193,26 @@ if __name__ == '__main__':
                                                                                opts.n_jobs, "Train data -", "train")
     dev_processed_sent1, dev_processed_sent2, dev_labels = get_sentences(nlp, dev_data, opts.batch_size, opts.n_jobs,
                                                                          "Dev data -", "dev")
-    #test_processed_sent1, test_processed_sent2, test_labels = get_sentences(nlp,test_data, opts.batch_size, opts.n_jobs, "Test data -", "test")
+    test_processed_sent1, test_processed_sent2, test_labels = get_sentences(nlp,test_data, opts.batch_size, opts.n_jobs, "Test data -", "test")
+
     train_features, dev_features, test_features = [], [], []
 
     for sent1, sent2 in tqdm(zip(train_processed_sent1, train_processed_sent2), desc='Prepare train features '):
-        train_features.append(features.exctract(sent1, sent2))
+        train_features.append(features.exctract(sent1, sent2, wandb.config))
 
     for sent1, sent2 in tqdm(zip(dev_processed_sent1, dev_processed_sent2), desc='Prepare dev features '):
-        dev_features.append(features.exctract(sent1, sent2))
+        dev_features.append(features.exctract(sent1, sent2, wandb.config))
+
+    for sent1, sent2 in tqdm(zip(test_processed_sent1, test_processed_sent2), desc='Prepare test features '):
+        test_features.append(features.exctract(sent1, sent2, wandb.config))
 
     model = get_model(wandb.config.model)
 
-    train_model(model, train_features, train_labels, dev_features, dev_labels, model.__class__.__name__, wandb)
+    clf = train_model(model, wandb)
+
+    wandb_classification_report(clf, dev_features, test_features, dev_labels, test_labels, wandb)
+
+
     # do your work here
     end_time = datetime.now()
     print('\n\nDuration: {}\n\n'.format(end_time - start_time))
